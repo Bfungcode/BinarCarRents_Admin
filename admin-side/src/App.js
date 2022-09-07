2635db2e4272d516f349cb6dabaaf986022f3c96
Caprysan <caprysan.sinaga@gmail.com>
2022-09-07 19:03:32 +0700

    adding branch, routing code only

--- admin-side/src/App.js
+++ admin-side/src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 
 const App = () => {
   return (
     // route menentukan URL
     <>
+    <Routes>  
+      {/* url "/" untuk login, */}
+      <Route path="/" element={<LoginPage />}/>
+      <Route path="/home" element={<Homepage />} /> 
+      <Route path='*' element={<NotFound />} />
+    </Routes>
+
       {/* <Routes>
         <Route path="/" element={<LandingPage />} />
 
