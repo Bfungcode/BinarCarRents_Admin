4b7b9df45c73c57e030b0a9e42c4c8e4fbf81cad
aliyah <aisyah@mmi-pt.com>
2022-09-09 08:50:45 +0700

    add: route page cars

--- admin-side/src/App.js
+++ admin-side/src/App.js
@@ -1,13 +1,16 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Cars from './pages/cars';
+import Dashboard from './pages/dashboard';
 
 const App = () => {
   return (
     // route menentukan URL
     <>
-      {/* <Routes>
-        <Route path="/" element={<LandingPage />} />
-
-      </Routes> */}
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/cars" element={<Cars />} />
+      </Routes>
     </>
   );
 };
