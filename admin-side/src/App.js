f807ca9484bf082f9393e1cef90df99c9cdcb21d
rleBcode <bfung0011@gmail.com>
2022-09-10 17:29:29 +0700

    FINISH

--- admin-side/src/App.js
+++ admin-side/src/App.js
@@ -1,13 +1,20 @@
 import './App.css';
+import './styles/LoginForm.css'
+import './styles/cars.css'
+import { Route, Routes } from 'react-router-dom'
+import Cars from './pages/cars';
+import Dashboard from './pages/dashboard';
+import LoginPage from './pages/LoginPage';
 
 const App = () => {
   return (
     // route menentukan URL
     <>
-      {/* <Routes>
-        <Route path="/" element={<LandingPage />} />
-
-      </Routes> */}
+      <Routes>
+        <Route path='/' element={<LoginPage />} />
+        <Route path='dashboard' element={<Dashboard />} />
+        <Route path='cars' element={<Cars />} />
+      </Routes>
     </>
   );
 };
---
+++ admin-side/src/vectors/svg-user.js
@@ -0,0 +1,42 @@
+const SVGUser = () => {
+    return (
+        <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <g clipPath="url(#clip0_40814_4682)">
+                <path
+                    d="M19.1667 17.5V15.8333C19.1661 15.0948 18.9203 14.3773 18.4678 13.7936C18.0153 13.2099 17.3818 12.793 16.6667 12.6083"
+                    stroke="#8A8A8A"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                />
+                <path
+                    d="M14.1667 17.5V15.8333C14.1667 14.9493 13.8155 14.1014 13.1904 13.4763C12.5652 12.8512 11.7174 12.5 10.8333 12.5H4.16668C3.28262 12.5 2.43478 12.8512 1.80965 13.4763C1.18453 14.1014 0.833344 14.9493 0.833344 15.8333V17.5"
+                    stroke="#222222"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                />
+                <path
+                    d="M13.3333 2.60833C14.0504 2.79191 14.6859 3.20891 15.1397 3.79359C15.5935 4.37826 15.8399 5.09735 15.8399 5.8375C15.8399 6.57764 15.5935 7.29673 15.1397 7.8814C14.6859 8.46608 14.0504 8.88308 13.3333 9.06666"
+                    stroke="#222222"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                />
+                <path
+                    d="M7.49999 9.16667C9.34094 9.16667 10.8333 7.67428 10.8333 5.83333C10.8333 3.99238 9.34094 2.5 7.49999 2.5C5.65904 2.5 4.16666 3.99238 4.16666 5.83333C4.16666 7.67428 5.65904 9.16667 7.49999 9.16667Z"
+                    stroke="#222222"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                />
+            </g>
+            <defs>
+                <clipPath id="clip0_40814_4682">
+                    <rect width="20" height="20" fill="white" />
+                </clipPath>
+            </defs>
+        </svg>
+    );
+};
+
+export default SVGUser;
