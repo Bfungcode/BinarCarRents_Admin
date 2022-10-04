5e1f13735b292c083a0351797775cd15742eea9b
BfungCode <105415086+rleBcode@users.noreply.github.com>
2022-10-04 21:28:05 +0700

    sd

--- admin-side/src/App.js
+++ admin-side/src/App.js
@@ -1,4 +1,3 @@
-import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import './styles/LoginForm.css'
 import './styles/cars.css'
--- admin-side/src/auth/auth-slice.js
+++ admin-side/src/auth/auth-slice.js
@@ -1,49 +1,49 @@
-import {createSlice, createAsyncThunk} from '@reduxjs/toolkit';
-import authAPI from './authAPI';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import authAPI from './auth-API';
 import { setMessage } from './message-slice';
 
 
 
 const user = JSON.parse(localStorage.getItem('user'));
 
-export const login = createAsyncThunk( "auth/login",
-  async({email, password}, thunkAPI)=> {
-    try{
+export const login = createAsyncThunk("auth/login",
+  async ({ email, password }, thunkAPI) => {
+    try {
       const data = await authAPI.login(email, password);
-      return {user : data};
+      return { user: data };
     }
     catch (err) {
       const message = (
         err.response && err.response.data && err.response.data.message) ||
         err.message || err.toString();
-    thunkAPI.dispatch(setMessage(message));
-    return thunkAPI.rejectWithValue();
+      thunkAPI.dispatch(setMessage(message));
+      return thunkAPI.rejectWithValue();
     }
   });
 
 
-  const initialState = user ? {isLoggedIn : true, user}:
-                      {isLoggedIn : false, user : null};
+const initialState = user ? { isLoggedIn: true, user } :
+  { isLoggedIn: false, user: null };
 //user && localstorage.getItem()
 
 
-  const authSlice = createSlice ({
-    name : "auth",
-    initialState,
-    extraReducers : {
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  extraReducers: {
 
-      [login.fulfilled] : (state, action) => {
-        state.isLoggedIn = true;
-        state.user = action.payload.user;
-        },
-      [login.rejected] : (state, action) => {
-          state.isLoggedIn = false;
-          state.user = null;
-        },
-    }
-  });
+    [login.fulfilled]: (state, action) => {
+      state.isLoggedIn = true;
+      state.user = action.payload.user;
+    },
+    [login.rejected]: (state, action) => {
+      state.isLoggedIn = false;
+      state.user = null;
+    },
+  }
+});
 
-const {reducer} = authSlice;
+const { reducer } = authSlice;
 
 export default authSlice.reducer;
 
