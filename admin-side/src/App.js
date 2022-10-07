c6483c0d94c5e5c336a92e3c6e8627d98a8271fa
aliyah <aisyah@mmi-pt.com>
2022-10-07 19:06:06 +0700

    add: routing page to add-edit car

--- admin-side/src/App.js
+++ admin-side/src/App.js
@@ -1,20 +1,24 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import './styles/LoginForm.css'
-import './styles/cars.css'
-import { Route, Routes } from 'react-router-dom'
-import Cars from './pages/cars';
+import CarAddEditPage from './pages/CarAddEditPage';
+import Cars from './pages/CarsPage';
+import DashboardPage from './pages/DashboardPage';
 import LoginPage from './pages/LoginPage';
-import DashboardPage from './pages/dashboard';
+import './styles/Cars.css';
+import './styles/LoginForm.css';
 
 const App = () => {
   return (
     // route menentukan URL
     <>
       <Routes>
-        <Route path='/' element={<LoginPage />} />
-        <Route path='dashboard' element={<DashboardPage />} />
-        <Route path='cars' element={<Cars />} />
+        <Route path="/" element={<LoginPage />} />
+        <Route path="dashboard" element={<DashboardPage />} />
+        <Route path="cars">
+          <Route index element={<Cars />} />
+          <Route path="add" element={<CarAddEditPage />} />
+          <Route path=":id" element={<CarAddEditPage />} />
+        </Route>
       </Routes>
     </>
   );
