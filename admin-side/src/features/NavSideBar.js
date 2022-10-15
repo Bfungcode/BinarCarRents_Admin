b384dc7859043172a7f9d2df207cba32de91954a
Caprysan <caprysan.sinaga@gmail.com>
2022-10-15 19:03:45 +0700

    adding nav and sidebar component, navigate on blue sidebar and update styling

--- admin-side/src/features/NavSideBar.js
+++ admin-side/src/features/NavSideBar.js
@@ -1,26 +1,83 @@
 import '../styles/NavandSideBar.css';
+import { useState } from 'react';
+import {NavLink} from 'react-router-dom';
+import { SVGDashboard, SVGDropdownMenu, SVGMenu, SVGSearch, SVGTruck } from '../vectors/navandsidebar-icon';
 
 const NavSideBar = ({ PageContent }) => {
+  const [isDashboard, setIsDashboard] = useState(true)
+  function onCars () {
+    setIsDashboard(false)
+  }
   return (
     <div className="Dashboard">
-      <div className="main-sidebar">
-        <div className="logo"> logo</div>
-        <div> icon1</div>
-        <p>subtitle</p>
-        <div> icon2</div>
-        <p>subtitle</p>
-      </div>
-      <div className="h-navbar">
-        <div className="logo">Binar-Car</div>
-        <div className="sidebar-content">
-          <div className="sidebar">
-            <ul>
-              <li>title1</li>
-              <li>title2</li>
-            </ul>
+        <div className="main-sidebar">
+          <div className="square-logo"> BR</div>
+          <div className='icon-dashboard'>
+            <NavLink to='/dashboard' activeClassName="active">
+              <SVGDashboard/>
+              <p>Dashboard</p>
+            </NavLink>
           </div>
-          <PageContent />
+          <div className='icon-cars' onClick={()=>onCars()}>
+            <NavLink to='/Cars'>
+              <SVGTruck/>
+              <p>Cars</p>
+            </NavLink>
+          </div>
         </div>
+    
+      <div className='right-layout'>
+      <div>
+          <div className="h-navbar">
+              <div className="nav-logo">Binar-Car</div>
+              <div><SVGMenu /></div>
+
+          <div className='searchbar'>
+                <div className="input-group searchgroup">
+                  <div className="form-outline search-input">
+                  <span>
+                    <SVGSearch />
+                  </span>
+                  <input type="search" id="form1" placeholder="Search"
+                  />
+                  <label className="form-label"></label>
+                  </div>
+                  <div className='search-button'>
+                    Search
+                  </div>
+              </div>
+          </div>
+          <div className='acct-group'>
+              <div className='acct-icon'>U</div>
+              <div className='acct-name'>Unis Badri</div>
+          </div>
+          <div className='dropdown'><SVGDropdownMenu /> </div>
+      </div>
+          <div className="sidebar-content">
+            
+            <div className="sidebar">
+              
+              
+              {isDashboard === true ? (
+
+              <div>
+                <div id='menu'>DASHBOARD</div>
+                <div id='menu'>Dashboard</div>
+              </div>  
+              ):
+              <div>
+              <div id='menu'>CARS</div>
+                <div id='menu'>List Car</div>
+                </div>
+              }
+              
+                
+            
+              
+            </div>
+            <div><PageContent /></div>
+          </div>
+          </div>
       </div>
     </div>
   );
---
+++ admin-side/src/vectors/navandsidebar-icon.js
@@ -0,0 +1,53 @@
+export const SVGTruck = () => {
+  return(
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M18.5 21C19.8807 21 21 19.8807 21 18.5C21 17.1193 19.8807 16 18.5 16C17.1193 16 16 17.1193 16 18.5C16 19.8807 17.1193 21 18.5 21Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M5.5 21C6.88071 21 8 19.8807 8 18.5C8 17.1193 6.88071 16 5.5 16C4.11929 16 3 17.1193 3 18.5C3 19.8807 4.11929 21 5.5 21Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M16 8H20L23 11V16H16V8Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M16 3H1V16H16V3Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>
+
+  )
+}
+
+export const SVGDropdownMenu =()=>{
+  return (
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M6 9L12 15L18 9" stroke="#151515" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>
+
+  )
+}
+
+export const SVGMenu = ()=> {
+  return(
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M3 18H21" stroke="#151515" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M3 12H21" stroke="#151515" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M3 6H21" stroke="#151515" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>
+
+  )
+}
+
+export const SVGSearch = ()=> {
+  return (
+    <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M8.25 14.25C11.5637 14.25 14.25 11.5637 14.25 8.25C14.25 4.93629 11.5637 2.25 8.25 2.25C4.93629 2.25 2.25 4.93629 2.25 8.25C2.25 11.5637 4.93629 14.25 8.25 14.25Z" stroke="#D0D0D0" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+      <path d="M15.75 15.75L12.4875 12.4875" stroke="#D0D0D0" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>
+
+  )
+}
+
+export const SVGDashboard = () => {
+  return(
+
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M3 9L12 2L21 9V20C21 20.5304 20.7893 21.0391 20.4142 21.4142C20.0391 21.7893 19.5304 22 19 22H5C4.46957 22 3.96086 21.7893 3.58579 21.4142C3.21071 21.0391 3 20.5304 3 20V9Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M9 22V12H15V22" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>
+    
+  )
+
+}
