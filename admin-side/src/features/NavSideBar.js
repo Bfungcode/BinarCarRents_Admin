b1f873b131baae9929b97b2c0bb09f696fd2a576
BfungCode <105415086+rleBcode@users.noreply.github.com>
2022-10-16 13:59:08 +0700

    DONE

--- admin-side/src/features/NavSideBar.js
+++ admin-side/src/features/NavSideBar.js
@@ -21,79 +21,53 @@ const NavSideBar = ({ PageContent }) => {
           <p>Cars</p>
         </div>
       </div>
-      <div className="right-layout">
-        <div className="h-navbar">
-          <div className="logo">Binar-Car</div>
-          {/* <div> <SVGMenu /> </div> */}
-          <div className="searchbar">
-            <div className="input-group">
-              <div className="form-outline">
-                <input type="search" id="form1" className="form-control" placeholder="Search" />
-                <label className="form-label"></label>
-              </div>
-              <div className="search-button">Search</div>
-            </div>
-          </div>
-          <div>
-            <div className="acct-icon">U</div>
-            <p>Unis Badri</p>
-          </div>
-          {/* <div><SVGDropdownMenu /> </div> */}
-        </div>
-        <div className="sidebar-content">
-          <div className="sidebar">
-            <ul>
-              <li>CARS</li>
-              <li>List Car</li>
-            </ul>
-          </div>
-        </div>
-        <div className='right-layout'>
-          <div>
-            <div className="h-navbar">
-              <div className="nav-logo">Binar-Car</div>
-              <div><SVGMenu /></div>
+      <div className='right-layout'>
+        <div>
+          <div className="h-navbar">
+            <div className="nav-logo">Binar-Car</div>
+            <div><SVGMenu /></div>
 
-              <div className='searchbar'>
-                <div className="input-group searchgroup">
-                  <div className="form-outline search-input">
-                    <span>
-                      <SVGSearch />
-                    </span>
-                    <input type="search" id="form1" placeholder="Search"
-                    />
-                    <label className="form-label"></label>
-                  </div>
-                  <div className='search-button'>
-                    Search
-                  </div>
+            <div className='searchbar'>
+              <div className="input-group searchgroup">
+                <div className="form-outline search-input">
+                  <span>
+                    <SVGSearch />
+                  </span>
+                  <input type="search" id="form1" placeholder="Search"
+                  />
+                  <label className="form-label"></label>
+                </div>
+                <div className='search-button'>
+                  Search
                 </div>
               </div>
-              <div className='acct-group'>
-                <div className='acct-icon'>U</div>
-                <div className='acct-name'>Unis Badri</div>
-              </div>
-              <div className='dropdown'><SVGDropdownMenu /> </div>
             </div>
-            <div className="sidebar-content">
-              <div className="sidebar">
-                {isDashboard === true ? (
-                  <div>
-                    <div id='menu'>DASHBOARD</div>
-                    <div id='menu'>Dashboard</div>
-                  </div>
-                ) :
-                  <div>
-                    <div id='menu'>CARS</div>
-                    <div id='menu'>List Car</div>
-                  </div>
-                }
-              </div>
-              <div><PageContent /></div>
+            <div className='acct-group'>
+              <div className='acct-icon'>U</div>
+              <div className='acct-name'>Unis Badri</div>
             </div>
+            <div className='dropdown'><SVGDropdownMenu /> </div>
+          </div>
+          <div className="sidebar-content">
+            <div className="sidebar">
+              {isDashboard === true ? (
+                <div>
+                  <div id='menu'>DASHBOARD</div>
+                  <div id='menu'>Dashboard</div>
+                </div>
+              ) :
+                <div>
+                  <div id='menu'>CARS</div>
+                  <div id='menu'>List Car</div>
+                </div>
+              }
+            </div>
+            <div><PageContent /></div>
           </div>
         </div>
       </div>
+    </div>
+  )
 }
 
-      export default NavSideBar;
+export default NavSideBar;
