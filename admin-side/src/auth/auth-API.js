9399928e56008e00797dc4c4a05a121ce48e7107
Bfung <bfung0011@gmail.com>
2023-09-25 09:40:10 +0700

    Update auth-API.js

--- admin-side/src/auth/auth-API.js
+++ admin-side/src/auth/auth-API.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 
-const API_URL = "https://bootcamp-rent-cars.herokuapp.com/";
+const API_URL = "https://api-car-rental.binaracademy.org/";
 
 const login = (email, password) => {
   return axios.post(`${API_URL}admin/auth/login`, {
